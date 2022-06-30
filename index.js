1e640c28cef6df6cd59c5fe53349b16617e9f1f7
DEV270201 <devansh.js@somaiya.edu>
2022-06-30 14:11:18 +0530

    connected database

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const {config} = require('dotenv');
 config({path : "./config.env"});
 const app = require("./app");
 
+const connect_database = ()=>{
+   try{ 
+       mongoose.connect(
+           process.env.DATABASE , {
+               useNewUrlParser: true,
+               useUnifiedTopology: true,
+           }
+       )
+       console.log("Database connected successfully!");
+   }catch(err){
+       console.log("Cannot connect to the database!");
+   }
+}
 
-let port = process.env.PORT | 4000;
+connect_database();
+
+let port = process.env.PORT || 4000;
 app.listen(port,()=>{
-   console.log(`server is listening on .....${port}`);
+   console.log(`server is listening on ${port}....`);
 });
+
