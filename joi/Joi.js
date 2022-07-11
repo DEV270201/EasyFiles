b92e40fa6a052c9bbcc5e5b6ae3afc4874343249
DEV270201 <devansh.js@somaiya.edu>
2022-07-11 15:32:22 +0530

    added regex for password and email

--- joi/Joi.js
+++ joi/Joi.js
@@ -17,3 +17,20 @@ exports.uploader = (req,res,next)=>{
       }
 }
 
+
+exports.RegisterJoi = async (body)=>{
+   const schema = joi.object({
+    //can use better regex than mine
+     email : joi.string().regex(/^[a-z]+\d*\.?[a-z\d]*@(gmail|hotmail|yahoo)\.(com|in)$/),
+     username : joi.string().max(25),
+    //can use better regex than mine
+     password : joi.string().min(8).max(15).regex(/^[a-zA-Z]+[a-zA-Z\d]*[@$#]+[a-zA-Z@$#\d]*\d+$/)
+   });
+
+   try{
+     return await schema.validateAsync(body);
+   }catch(err){
+    console.log("register joi : ",err);
+    throw err;
+   }
+}
