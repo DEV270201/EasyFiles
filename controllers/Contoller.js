b9ddeeae954a03e87e796d0647cdd4ecb85d62ac
DEV270201 <devansh.js@somaiya.edu>
2022-07-11 16:49:30 +0530

    hashed the password

--- controllers/Contoller.js
+++ controllers/Contoller.js
@@ -1,6 +1,7 @@
 const { ClientError } = require('../handlers/Error');
 const File = require('../models/File');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.Uploader = async(req,filename)=>{
     try{
@@ -18,6 +19,10 @@ exports.Uploader = async(req,filename)=>{
 
 exports.RegisterUser = async(user)=>{
   try{
+    //hashing the password
+     const salt = await bcrypt.genSalt(10);
+     user.password = await bcrypt.hash(user.password, salt);
+     
      await User.create({email : user.email,username : user.username,password : user.password});
      return;
   }catch(err){
