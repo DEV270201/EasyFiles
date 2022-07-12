20ec6adc4a8e531ee8b41b6749967c414b4ce527
DEV270201 <devansh.js@somaiya.edu>
2022-07-12 12:28:22 +0530

    routing partially done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from './Navbar';
+import { Switch,Route,Redirect,withRouter } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
 
 const App = ()=>{
    return(
     <>
-    <h1>Client here..</h1>
+    <Navbar />
+    <Switch>
+        <Route exact component={withRouter(Home)} path="/"  />
+        <Route exact component={withRouter(Register)} path="/user/register"/>
+        <Route exact component={withRouter(Login)} path="/user/login"/>
+        <Redirect to="/" />
+    </Switch>
     </>
    );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mynavbar">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" to="/">yourMinePDF</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" aria-current="page" to="/" activeStyle={{color : "red"}}>Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/user/register">Register</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/user/login">Login</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
