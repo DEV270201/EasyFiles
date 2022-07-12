e89bfd5ddb39521a6d3ee18920afb8d21ceabae9
DEV270201 <devansh.js@somaiya.edu>
2022-07-12 18:10:09 +0530

    completed login UI

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Login = ()=>{
-    return(
+const Login = () => {
+    return (
         <>
-         <h1>login</h1>
+            <div className="container p-3">
+                <div className="outer">
+                    <h4 className="text-center font-weight-light mt-1 mb-2">Login Yourself...</h4>
+                    <div className="inputs">
+                        <label htmlFor="Username">Username*</label>
+                        <input type="text" className="form-control myform" id="Username" aria-describedby="emailHelp" name="username" required/>
+                        <label htmlFor="Password">Password*</label>
+                        <input type="password" className="form-control myform" id="Password" name="password" required/>
+                        <button className="btn btn-outline-dark mybtn mt-3">Login</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
-
 export default Login;
