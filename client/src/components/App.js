9961cdf96e46c84b3e56cff3076f270e9d3d6474
DEV270201 <devansh.js@somaiya.edu>
2022-07-13 19:43:42 +0530

    added file uploader UI

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { Switch,Route,Redirect,withRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Home from './Home';
 import Footer from './Footer';
+import LoginContextProvider from '../context/LoginContext';
+import Upload from './Upload';
 
-const App = ()=>{
-   return(
-    <>
-    <Navbar />
-    <Switch>
-        <Route exact component={withRouter(Home)} path="/"  />
-        <Route exact component={withRouter(Register)} path="/user/register"/>
-        <Route exact component={withRouter(Login)} path="/user/login"/>
-        <Redirect to="/" />
-    </Switch>
-    <Footer />
-    </>
-   );
+const App = () => {
+    return (
+        <>
+            <LoginContextProvider>
+                <Navbar />
+                <Switch>
+                    <Route exact component={withRouter(Home)} path="/" />
+                    <Route exact component={withRouter(Upload)} path="/upload" />
+                    <Route exact component={withRouter(Register)} path="/user/register" />
+                    <Route exact component={withRouter(Login)} path="/user/login" />
+                    <Redirect to="/" />
+                </Switch>
+            </LoginContextProvider>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/components/Upload.js
@@ -0,0 +1,78 @@
+import React,{useRef,useState,useContext,useEffect} from "react";
+import Swal from 'sweetalert2';
+import {useHistory} from 'react-router-dom';
+import axios from "axios";
+import { LoginContext } from "../context/LoginContext";
+
+//Here I am going to use uncontrolled components because I don't feel the of need controlled components.
+//You can change based on your needs and requirements.
+
+const Upload = () => {
+    console.log("upload page...");
+    const fref = useRef(null);
+    const fnref = useRef(null);
+    const history = useHistory();
+    const [load,setLoad] = useState(false);
+    const {isLoggedIn} = useContext(LoginContext);
+
+    useEffect(()=>{
+       if(!isLoggedIn){
+        history.push('/user/login');
+       }
+    },[]);
+
+    const upload  = async ()=>{
+       try{
+        console.log("file : ",fref.current.files[0]);
+       setLoad(true);
+         let response = await axios.post('/files',{
+            filename : fnref.current.value,
+            file : fref.current.files[0]
+         },{
+            headers : {
+                "Content-Type" : "multipart/form-data"
+            }
+         });
+         if(response.data.status === 'success'){
+            Swal.fire({
+                icon: 'success',
+                title: 'Yayy...',
+                text: response.data.msg
+              });
+             history.push("/");
+        }
+       }catch(err){
+        console.log("Login err : ",err);
+        setLoad(false);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: err.response.data.error
+            });
+            return;
+       }
+    }
+
+    return (
+        <>
+            <div className="container p-3">
+                <div className="outer">
+                    <h4 className="text-center font-weight-light mt-1 mb-2">Upload your PDF here...</h4>
+                    <div className="inputs">
+                        <label htmlFor="file">File*</label>
+                        <input type="file" className="form-control myform" id="file" aria-describedby="emailHelp" name="file" ref={fref} required/>
+                        <label htmlFor="Filename">File Name*</label>
+                        <input type="password" className="form-control myform" id="Filename" name="filename" ref={fnref} required/>
+                        <button className="btn btn-outline-dark mybtn mt-3" disabled={load ? true : false} onClick={upload}>{load ? 'Loading...' : 'Upload'}</button>
+                        {/* <button className="btn btn-outline-dark mybtn mt-3" onClick={hello}>Click</button> */}
+                    </div>
+                    <div className="mt-3">
+                    <div>NOTE : </div>
+                    <div>1] Name of the file should be free from the special characters.</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Upload;
